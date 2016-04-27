259ee0cef1e3e450659d9b3fadf5b6dc38637b52
Novopia <tomyao0@gmail.com>
2016-04-26 22:08:02 -0400

    output scraping to new csv

--- data_mining/github_scraper.py
+++ data_mining/github_scraper.py
@@ -1,30 +1,56 @@
 # We need Python 3 to run this
+# This should take about 10 minutes to run it
 import urllib.request
 from re import findall
+import csv
 
 
 def scraping_page(url):
     response = urllib.request.urlopen(url)
     html = response.read()
     html_str = html.decode()
-    num_watchers = int(\
-        findall(r'<a\sclass="[a-zA-z]+.*" href="/[a-zA-z-]+./[a-aA-z]*.watchers">[\s]*([0-9]+)[\s]*</a>', html_str)[0])
-    num_pull_requests = int(\
-        findall(r'<span\sitemprop="name">Pull requests</span>\s*<span class="counter">([0-9]+)</span>', html_str)[0])
 
-    num_commits = int(\
-        findall(r'<a\sdata-pjax href="[A-za-z]*.*\s*<svg\s.*>\s*<[a-z]*.+>\s*([0-9]+)\s*</span>\s*commits\s*</a>', html_str)[0])
+    regex = findall(r'<a\s*class="[^ ]*" href="[^ ]*watcher[s]*">[\s]*([0-9]+)[\s]*</a>', html_str)
+    if len(regex) > 0:
+        num_watchers = int(regex[0])
+    else:
+        num_watchers = -1
 
-    num_contributors = int(\
-        findall(r'<a href="[A-za-z]*.*\s*<svg\s.*>\s*<[a-z]*.+>\s*([0-9]+)\s*</span>\s*contributors\s*</a>', html_str)[0])
-    print(num_watchers)
-    print(num_pull_requests)
-    print(num_commits)
-    print(num_contributors)
+    regex = findall(r'<span\s*itemprop="name">Pull requests</span>\s*<span class="counter">([0-9]+)</span>', html_str)
+    if len(regex) > 0:
+        num_pull_requests = int(regex[0])
+    else:
+        num_pull_requests = -1
+
+    regex = findall(r'<a\s*data-pjax[^ ]*\s*href="[^ ]*">\s*<svg.*</svg>\s*<span[^>]*>\s*([0-9]+)\s*</span>\s*commit[s]*\s*</a>', html_str)
+    if len(regex) > 0:
+        num_commits = int(regex[0])
+    else:
+        num_commits = -1
+
+    regex = findall(r'<a\shref="[A-za-z]*.*\s*<svg\s.*>\s*<[a-z]*.+>\s*([0-9]+)\s*</span>\s*contributor[s]*\s*</a>', html_str)
+    if len(regex) > 0:
+        num_contributors = int(regex[0])
+    else:
+        num_contributors = -1
+
+    return [num_watchers, num_pull_requests, num_contributors, num_commits]
 
 
 def main():
-    scraping_page("https://github.com/A-limon/pacman")
+    with open("repo.csv") as in_file:
+        with open("repo_with_scraping.csv", "w") as out_file:
+            csv_writer = csv.writer(out_file)
+            csv_reader = csv.reader(in_file)
+            first_line = True
+            for line in csv_reader:
+                if first_line:
+                    first_line = False
+                    csv_writer.writerow(line + ['watchers', 'pull_requests', 'contributors', 'commits'])
+                else:
+                    scrape_result = scraping_page(line[2])
+                    csv_writer.writerow(line + scrape_result)
+
 
 
 if __name__ == '__main__':
