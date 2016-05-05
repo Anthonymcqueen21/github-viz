d85879ac01143798ef61b44f13be4ceb830a2c54
Novopia <tomyao0@gmail.com>
2016-05-04 23:57:24 -0400

    Gtrend_fetcher now supports command line

--- data_mining/scripts(webapp_version)/google_trends_fetcher.py
+++ data_mining/scripts(webapp_version)/google_trends_fetcher.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-
+import sys
 from pytrends.pyGTrends import pyGTrends
 import time
 from random import randint
@@ -9,42 +9,44 @@ google_username = "githubviz0@gmail.com"
 google_password = "githubvizcs1951"
 path = ""
 
-name = "Dijkstra"
-csv_name = name + "_trend"
-cleaned_csv_name = name + "_trend_cleaned.csv"
 
-# connect to Google
-connector = pyGTrends(google_username, google_password)
+def main():
+    name = sys.argv[1]
+    csv_name = name + "_trend"
+    cleaned_csv_name = name + "_trend_cleaned.csv"
 
-# make request
-connector.request_report(name)
+    # connect to Google
+    connector = pyGTrends(google_username, google_password)
 
-# wait a random amount of time between requests to avoid bot detection
-time.sleep(randint(5, 10))
+    # make request
+    connector.request_report(name)
 
-# download file
-connector.save_csv(path, csv_name)
+    # wait a random amount of time between requests to avoid bot detection
+    time.sleep(randint(5, 10))
 
-with open(csv_name + ".csv") as in_file:
-    csv_reader = csv.reader(in_file)
+    # download file
+    connector.save_csv(path, csv_name)
 
-    for i in range(5):
-        next(csv_reader)
-    dates_mapping = defaultdict(int)
-    for line in csv_reader:
-        if not line:
-            break
-        dateString = line[0]
-        dates = dateString.split(' - ')
-        start_dates = dates[0].split('-')
-        start_year = start_dates[0]
-        start_month = start_dates[1]
-        dates_mapping[start_year+"/"+start_month] += int(line[1])
+    with open(csv_name + ".csv") as in_file:
+        csv_reader = csv.reader(in_file)
 
-    with open(cleaned_csv_name, "w") as out_file:
-        csv_writer = csv.writer(out_file)
-        for item in sorted(dates_mapping.items()):
-            csv_writer.writerow([item[0], item[1]])
+        for i in range(5):
+            next(csv_reader)
+        dates_mapping = defaultdict(int)
+        for line in csv_reader:
+            if not line:
+                break
+            dateString = line[0]
+            dates = dateString.split(' - ')
+            start_dates = dates[0].split('-')
+            start_year = start_dates[0]
+            start_month = start_dates[1]
+            dates_mapping[start_year + "/" + start_month] += int(line[1])
 
+        with open(cleaned_csv_name, "w") as out_file:
+            csv_writer = csv.writer(out_file)
+            for item in sorted(dates_mapping.items()):
+                csv_writer.writerow([item[0], item[1]])
 
-
+if __name__ == '__main__':
+    main()
